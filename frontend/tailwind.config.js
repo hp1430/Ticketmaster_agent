2549417a372d6eff0dd76bdde810e374dee4f60b
Himanshu Parashar <parasharhimanshu41@gmail.com>
2026-09-23 10:55:22 +0530

    added tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindcss()],
+};
